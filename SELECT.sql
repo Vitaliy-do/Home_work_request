fede2400fd47d853e93bc0d1481635dd98281ed5
Vitaliy-do <4810672@gmail.com>
2025-09-06 16:27:41 +0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,54 +1,54 @@
 -- Задание 2
 
--- Запрос вывода названия и продолжительности самого длительного трека.
+-- Запрос 1 вывода названия и продолжительности самого длительного трека.
 select title_track, duration
 from track
 where duration = (select max(duration) from track);
 
--- Запрос вывода названия треков, продолжительность которых не менее 3,5 минут.
+-- Запрос 2 вывода названия треков, продолжительность которых не менее 3,5 минут.
 SELECT title_track
 FROM track
 WHERE duration >= 3.5;
 
--- Запрос вывода названия сборников, вышедших в период с 2018 по 2020 год включительно.
+-- Запрос 3 вывода названия сборников, вышедших в период с 2018 по 2020 год включительно.
 SELECT title_mix
 FROM mix
 WHERE year >= '2018' AND year <= '2020';
 
--- Запрос вывода исполнителей, чьё имя состоит из одного слова.
+-- Запрос 4 вывода исполнителей, чьё имя состоит из одного слова.
 SELECT name
 FROM executere
 WHERE name NOT LIKE '% %';
 
--- Запрос вывода названий треков, которые содержат слово «мой» или «my».
+-- Запрос 5 вывода названий треков, которые содержат слово «мой» или «my».
 SELECT title_track 
 FROM track 
 WHERE title_track LIKE '%Мой%';
 
 -- Задание 3
--- Запрос вывода количества исполнителей в каждом жанре.
+-- Запрос 6 вывода количества исполнителей в каждом жанре.
 SELECT g.title, COUNT(DISTINCT ge.executere_id) FROM genre g
 JOIN genreexecutere ge ON g.genre_id = ge.genre_id
 GROUP BY title;
 
--- Запрос вывода количества треков, вошедших в альбомы 2019–2020 годов.
+-- Запрос 7 вывода количества треков, вошедших в альбомы 2019–2020 годов.
 SELECT count(title_track) FROM track t 
 JOIN albom a ON t.albom_id = a.albom_id 
 WHERE year >= '2019' AND year <= '2020'; 
 
--- Запрос вывода средней продолжительности треков по каждому альбому.
+-- Запрос 8 вывода средней продолжительности треков по каждому альбому.
 SELECT a.title_albom, avg(duration) FROM track t
 JOIN albom a ON a.albom_id = t.albom_id
 GROUP BY a.title_albom;
 
--- Запрос вывода всех исполнителей, которые не выпустили альбомы в 2020 году.
+-- Запрос 9 вывода всех исполнителей, которые не выпустили альбомы в 2020 году.
 select e.name from executere e
 WHERE NOT EXISTS (select FROM albomexecutere al 
 JOIN albom a ON al.albom_id = a.albom_id
 WHERE al.executere_id = e.executere_id
 AND a.year = 2020);
 
--- Запрос вывода названий сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+-- Запрос 10 вывода названий сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 SELECT distinct title_mix FROM mix m 
 JOIN trackmix tm ON m.mix_id = tm.mix_id
 JOIN track t ON tm.track_id = t.track_id 
